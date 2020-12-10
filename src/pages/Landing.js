61353315e6de90e4b9d1b8413e11b91f5eef3169
LAPTOP-2KEC0QRP\Julian Bass <julianterrybass@gmail.com>
2020-12-10 21:37:49 +0800

    Adds Council section with flex styling

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,12 +5,14 @@ import React from 'react';
 // Page Components
 import AboutUs from '../components/for-landing/AboutUs';
 import Statements from '../components/for-landing/Statements';
+import Council from '../components/for-landing/Council';
 
 const Landing = () => {
     return (
         <div>
             <AboutUs />
             <Statements />
+            <Council />
         </div>
     )
 }
