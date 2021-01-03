f67951166429d793c21dd6f6ec9f4481e9f49621
LAPTOP-2KEC0QRP\Julian Bass <julianterrybass@gmail.com>
2021-01-03 16:19:27 +0800

    Adds search functionality for FAQ list

--- src/components/for-faq/FaqSection.js
+++ src/components/for-faq/FaqSection.js
@@ -1,6 +1,6 @@
 // The list of FAQs on the FAQ page
 
-import React from 'react';
+import React, { useState } from 'react';
 // Importing Styled Components
 import styled from 'styled-components';
 // Importing "DropToggle" Component for toggling functionality
@@ -13,14 +13,29 @@ import { AnimateSharedLayout } from 'framer-motion';
 import { FaqData } from './FaqData';
 
 const FaqSection = () => {
+
+    // State
+    // Creating a "questions" state holding all the FAQ Data
+    const [questions, setQuestions] = useState(FaqData);
+    // Creating state for the search term
+    const [searchTerm, setSearchTerm] = useState("");
+
+    // A variable for the filtered FAQ list holding the search term
+    const filteredFAQ = questions.filter(question => {
+        return question.faqTitle.toLowerCase().includes(searchTerm.toLowerCase())
+    });
+
     return (
         <FaqSectionStyle>
+            {/* Any changes to the input are set as the search term state */}
+            <input type="text" placeholder="Search an FAQ" onChange={e => setSearchTerm(e.target.value)} />
             <AnimateSharedLayout>
                 {/* Each DropToggle element is a clickable dropdown section */}
-                {/* This function maps through the FAQ Data JS file,
+                {/* This function maps through the filtered questions state 
+                which gets data from the FAQ Data JS file,
                 and returns a styled toggle component with that specific
                 array object's information and index. */}
-                {FaqData.map((question, index) => {
+                {filteredFAQ.map((question, index) => {
                     return (
                         <DropToggle title={question.faqTitle} key={index}>
                             <div className="drop-content">
@@ -64,6 +79,20 @@ const FaqSectionStyle = styled.div`
         p {
             padding: 1rem 0rem;
             line-height: 2rem;
+        }
+    }
+    input {
+        display: block;
+        width: 50%;
+        margin: 0 auto 2rem auto;
+        font-size: 1.5rem;
+        padding: 0.5rem;
+        border: none;
+        outline: none;
+    }
+    @media (max-width: 870px) {
+        input {
+            width: 100%;
         }
     }
     @media (max-width: 660px) {
