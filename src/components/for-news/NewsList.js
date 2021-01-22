5860094beb9a5625fd9819151d283838eb6e8b7a
LAPTOP-2KEC0QRP\Julian Bass <julianterrybass@gmail.com>
2021-01-22 21:17:12 +0800

    Styling for News Announcement List

--- src/components/for-news/NewsList.js
+++ src/components/for-news/NewsList.js
@@ -3,17 +3,58 @@
 import React from 'react';
 // Importing Components
 import NewsSummary from './NewsSummary';
+// Importing Styled Components
+import styled from 'styled-components';
+// Importing Framer Motion and Animations
+import { motion } from 'framer-motion';
+import { revealUp } from '../../assets/Animations';
 
 const NewsList = ({newsItems}) => {
     return (
-        <>
-            {newsItems && newsItems.map(newsItem => {
-                return (
-                    <NewsSummary newsItem={newsItem} key={newsItem.id} />
-                )
-            })}
-        </>
+        <MainContainer>
+            <Hide>
+                <HeaderSection variants={revealUp}>
+                    <h2>Browse the latest updates from Barangay</h2>
+                    <div className="divider"></div>
+                </HeaderSection>
+            </Hide>
+            <NewsSection>
+                {newsItems && newsItems.map(newsItem => {
+                    return (
+                        <NewsSummary newsItem={newsItem} key={newsItem.id} />
+                    )
+                })}
+            </NewsSection>
+        </MainContainer>
     )
 }
+
+// Styled Components
+
+const MainContainer = styled.div`
+    min-height: 90vh;
+    padding: 5rem 10rem;
+`
+const Hide = styled.div`
+    margin-bottom: 5rem;
+    height: 25vh;
+    overflow: hidden;
+`
+
+const HeaderSection = styled(motion.div)`
+    h2 {
+        margin-bottom: 1rem;
+    }
+    .divider {
+        width: 100%;
+        height: 0.3rem;
+        background: white;
+    }
+`
+
+
+const NewsSection = styled.div`
+
+`
 
 export default NewsList;
--- src/components/for-news/NewsSummary.js
+++ src/components/for-news/NewsSummary.js
@@ -1,14 +1,35 @@
 // Component for summarized card of news information
 
 import React from 'react';
+// Importing Styled Components
+import styled from 'styled-components';
 
 const NewsSummary = ({newsItem}) => {
     return (
-        <>
-            <h2>{newsItem.heading}</h2>
+        <NewsCard>
+            <h4>{newsItem.heading}</h4>
+            <div className="divider"></div>
             <p>{newsItem.body}</p>
-        </>
+        </NewsCard>
     )
 }
+
+// Styled Components
+
+const NewsCard = styled.div`
+    margin-bottom: 2.5rem;
+    padding: 1.5rem;
+    background: #071b3d;
+    border-radius: 2rem;
+    h4 {
+        font-weight: bold;
+        margin-bottom: 0.5rem;
+    }
+    .divider {
+        width: 7%;
+        height: 0.5rem;
+        background: #fde00d;
+    }
+`
 
 export default NewsSummary;
