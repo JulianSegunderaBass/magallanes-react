941c7eb145bd27d4418206424c935ce9a38bc9d8
LAPTOP-2KEC0QRP\Julian Bass <julianterrybass@gmail.com>
2021-02-04 20:06:57 +0800

    Fixed navbar edit

--- src/redux-store/reducers/NewsReducer.js
+++ src/redux-store/reducers/NewsReducer.js
@@ -5,17 +5,24 @@ const initState = {
         {id: '1', heading: 'A new website is up', body: 'A website designed and developed by APC students for Barangay Magallanes is now up and running.'},
         {id: '2', heading: 'New website uses React and Firebase', body: 'The APC Team has implemented a Firebase database for the website.'},
         {id: '3', heading: 'Other features planned to be released for website.', body: 'Newer features are currently under development for the Barangay Information Site.'}
-    ]
+    ],
+    newsAnnouncementError: null
 }
 
 const NewsReducer = (state = initState, action) => {
     switch (action.type) {
         case 'CREATE_ANNOUNCEMENT':
             console.log('Created Announcement: ', action.payload);
-            return state;
+            return {
+                ...state,
+                newsAnnouncementError: null
+            }
         case 'CREATE_ANNOUNCEMENT_ERROR':
             console.log('Create announcement error', action.error);
-            return state;
+            return {
+                ...state,
+                newsAnnouncementError: action.error.message
+            }
         default:
             return state;
     }
