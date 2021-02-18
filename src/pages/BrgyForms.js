ad8ecb89fdfc6bf22681f54c682d3bc276dc4691
LAPTOP-2KEC0QRP\Julian Bass <julianterrybass@gmail.com>
2021-02-18 20:35:49 +0800

    Changes BrgyForm.js styling to use styled components

--- src/pages/BrgyForms.js
+++ src/pages/BrgyForms.js
@@ -1,23 +1,39 @@
-import React from "react";
+// Embedded Google Form
 
-import "./BrgyForms.css";
+import React from "react";
+// Importing Styled Components
+import styled from 'styled-components';
 
 const BrgyForms = () => {
   return (
-    <div className='form-container'>
-      <iframe
+    <FormContainer>
+      <FormIframe
         src="https://docs.google.com/forms/d/e/1FAIpQLScYarwDi-xSpRAadDFt047ZhpTu2YDJFzrclVG_ue8uPf4opA/viewform?embedded=true"
         // width="640"
         // height="1500"
         frameborder="0"
         marginheight="0"
         marginwidth="0"
-        className="form-iframe"
       >
         Loading…
-      </iframe>
-    </div>
+      </FormIframe>
+    </FormContainer>
   );
 };
+
+// Styled Components
+
+const FormContainer = styled.div`
+  width: 100%;
+  height: 1500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const FormIframe = styled.iframe`
+  flex: 1;
+  height: 100%;
+`
 
 export default BrgyForms;
