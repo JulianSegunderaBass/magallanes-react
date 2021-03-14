c4975975ba3885d8e867871c6760cdbcf2902275
LAPTOP-2KEC0QRP\Julian Bass <julianterrybass@gmail.com>
2021-03-14 20:39:50 +0800

    Configuration edits

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -7,13 +7,13 @@ import 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyBRmrqnqcNcnXzeS9-cpeecHGmLt6KgQ3U",
-    authDomain: "magallanes-react.firebaseapp.com",
-    projectId: "magallanes-react",
-    storageBucket: "magallanes-react.appspot.com",
-    messagingSenderId: "457879947670",
-    appId: "1:457879947670:web:d3bc54851cacb424a1b5b7",
-    measurementId: "G-S808R0S3MS"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
